345a44e082795070155c8cbd10cd238ddf73f70f
Kevin Cristiano <kcristiano@tadpole.cc>
2017-08-06 17:48:59 -0400

    civicrm-47: 4.7.23 release

--- civicrm/vendor/autoload.php
+++ civicrm/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInita288f404cef1fb6e72cf5dc6d74b004a::getLoader();
+return ComposerAutoloaderInit7282ccb8905e9aeacaba1c5a3b7b523b::getLoader();
