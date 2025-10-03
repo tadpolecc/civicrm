96f9ff89b8b50b868465e765d563177359a98b5d
Christian Wach <needle@haystack.co.uk>
2025-10-03 09:31:45 +0100

    Avoid starting a PHP session unless necessary

--- civicrm.php
+++ civicrm.php
@@ -487,9 +487,6 @@ class CiviCRM_For_WordPress {
       wp_die(__('Only one instance of CiviCRM_For_WordPress please', 'civicrm'));
     }
 
-    // Maybe start session.
-    $this->maybe_start_session();
-
     /*
      * AJAX calls do not set the 'cms.root' item, so make sure it is set here so
      * the CiviCRM doesn't fall back on flaky directory traversal code.
@@ -712,6 +709,9 @@ class CiviCRM_For_WordPress {
 
     // Store.
     self::$context = $context;
+
+    // Maybe start session.
+    $this->maybe_start_session();
 
   }
 
